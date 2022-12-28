2896bfa12128a518cdfa35c6f19cf32caaa1a065
Furkan3525 <120331839+Furkan3525@users.noreply.github.com>
2022-12-28 17:36:34 +0300

    Add files via upload

---
+++ PersonelApp/models/PersonelCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace PersonelApp.Models
+{
+    public partial class PersonelCategory
+    {
+        public int Id { get; set; }
+        public int? UserId { get; set; }
+        public string CategoryName { get; set; }
+        public int? WeeklyWorkTime { get; set; }
+
+        public virtual Personel User { get; set; }
+    }
+}
