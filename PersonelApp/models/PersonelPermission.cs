0a8ab3bb7380c4d8ea3642daff09e6095a25471c
Furkan3525 <120331839+Furkan3525@users.noreply.github.com>
2022-12-28 17:36:04 +0300

    Add files via upload

---
+++ PersonelApp/models/PersonelPermission.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace PersonelApp.Models
+{
+    public partial class PersonelPermission
+    {
+        public int Id { get; set; }
+        public int? UserId { get; set; }
+        public int? PermissionHour { get; set; }
+        public string Description { get; set; }
+        public virtual Personel User { get; set; }
+    }
+}
